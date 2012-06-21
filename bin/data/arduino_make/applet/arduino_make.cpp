16bbb85a2f72024908f192df6b49301c541090e1
Aaron Heidgerken <aheidgerken@smm.org>
2012-06-21 14:01:57 -0500

    Updated the gitignore to grab the data files

--- bin/data/arduino_make/applet/arduino_make.cpp
+++ bin/data/arduino_make/applet/arduino_make.cpp
@@ -9,8 +9,9 @@ bool thru=false;
 void setup(){
 	thru=false;
 	  (jack1).mode(INPUT);
+	  (jack3).mode(SERVO);
 	  (jack2).mode(OUTPUT);
-	  
+	    
 }
 
 void loop(){
@@ -18,14 +19,20 @@ void loop(){
 	
 	
 	
+	
+	
 
 	if(!thru){
 			while(1){
-			if((jack1).dChanged()){
-				(jack2).dWrite(1);
-				delay(0.5 *1000);
-			}
+			while((jack1).dRead()==0);
+			  delay(10);
+			  while((jack1).dRead()==1);
+			(jack3).sWrite(0);
+			delay(0.5 *1000);
+			(jack2).dWrite(1);
+			delay(0.5 *1000);
 			(jack2).dWrite(0);
+			(jack3).sWrite(180);
 		}
 
 		thru=true;
