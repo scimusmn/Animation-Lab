69f89b603ed0f746314c08ad4f9afcac9dcd4635
Aaron Heidgerken <aheidgerken@smm.org>
2012-08-29 15:26:03 -0500

    Changes for .gitignore

--- bin/data/arduino_make/applet/arduino_make.cpp
+++ bin/data/arduino_make/applet/arduino_make.cpp
@@ -8,16 +8,32 @@ bool thru=false;
 
 void setup(){
 	thru=false;
-	  (port1).mode(INPUT);
-
+	  (port4).mode(OUTPUT);
+	  (port3).mode(INPUT);
+	  (port1).mode(OUTPUT);
+	      (port2).mode(INPUT);
+	            
 }
 
 void loop(){
 
 	if(1){
-			while((port1).dRead()==0);
-		  delay(10);
-		  while((port1).dRead()==1);
+		(port4).dWrite(1);
+		if((port3).dRead()==1){
+			(port1).dWrite(1);
+		}
+		if((port3).dRead()==0){
+			(port1).dWrite(0);
+		}
+		delay(map((port2).aRead(),0,1024,0,2000));
+		(port4).dWrite(0);
+		if((port3).dRead()==1){
+			(port1).dWrite(1);
+		}
+		if((port3).dRead()==0){
+			(port1).dWrite(0);
+		}
+		delay(map((port2).aRead(),0,1024,0,2000));
 
 		//thru=true;
 	}
